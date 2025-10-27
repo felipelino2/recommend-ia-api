de64629b8765ddac73a0518e1528202a7eaef504
Olinoconta2 <felipelinodesigndoe@gmail.com>
2025-10-27 11:48:58 -0200

    jo

--- core/prompts.py
+++ core/prompts.py
@@ -6,10 +6,6 @@ BASE_DIR = Path(__file__).resolve().parent
 path_structured = BASE_DIR / "json_files_prompt" / "structured.json"
 path_textual    = BASE_DIR / "json_files_prompt" / "textual.json"
 
-STRUCTURED = read_json_file(path_structured)  # deixe como dict
-TEXTUAL    = read_json_file(path_textual)     # deixe como dict
-
-
 STRUCTURED = str(read_json_file(path_structured))
 TEXTUAL = str(read_json_file(path_textual))
 
